d362a88164b8cc67eb2cbf1a787048532b222438
MikHM <hemroda.martin@gmail.com>
2017-02-10 13:35:41 +0100

    updating SecurityController in order to fix routing to '/login' issue

--- src/Framaru/CMSBundle/Controller/SecurityController.php
+++ src/Framaru/CMSBundle/Controller/SecurityController.php
@@ -1,13 +1,14 @@
 <?php
 
+
 namespace Framaru\CMSBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class SecurityController extends Controller
 {
     /**
      * @Route("/login", name="login")
@@ -22,17 +23,10 @@ class DefaultController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render("FramaruCMSBundle:security/login.html.twig", array(
+        return $this->render("@CMS/Security/login.html.twig", array(
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
     }
 
-    /**
-     * @Route("/admin")
-     */
-    public function adminAction()
-    {
-        return new Response('<html><body>Admin page!</body></html>');
-    }
 }
